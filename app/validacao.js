9998458dbeab7e5a7ee7807ec2224998e2efb0f7
PedroPassos081 <pedro.passos081@gmail.com>
2023-09-26 12:54:45 -0300

    deixando o jogo mais dinamico

--- app/validacao.js
+++ app/validacao.js
@@ -2,12 +2,32 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute
 
     if (chuteForInvalido(numero)) {
-        console.log('número válido')
+        elementoChute.innerHTML +='<div>Valor inválido</div>'
+        return
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        console.log(`valor inválido: o número secreto precisa 
-        estar entre ${menorValor} e ${maiorValor}`)
+        elementoChute.innerHTML += `<div>valor inválido: Fale um número
+         entre ${menorValor} e ${maiorValor}</div>`
+         return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor 
+        <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    } else { 
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior 
+        <i class="fa-solid fa-arrow-up"></i></div>
+        `
+
     }
 }
 
